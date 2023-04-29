dbb66b963f56d27276aca4c4effeb6bad6ff8c01
BEN ELHAJ SALAH Sami <sami.bhs.91@gmail.com>
2023-04-29 23:52:48 +0200

    add build file

---
+++ build/CMakeFiles/monprojet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monprojet.dir/main.cpp.o"
+  "CMakeFiles/monprojet.dir/main.cpp.o.d"
+  "monprojet"
+  "monprojet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monprojet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
