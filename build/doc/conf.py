43a203b416c73871dc95a69bed095d83adfe53ce
BEN ELHAJ SALAH Sami <sami.bhs.91@gmail.com>
2023-05-01 17:38:06 +0200

    add example sphinx doc

---
+++ build/doc/conf.py
@@ -0,0 +1,23 @@
+# Configuration file for Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'Mon Projet'
+author = 'Mon Auteur'
+version = '1.0'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+#html_theme = 'sphinx_rtd_theme'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_static_path = ['_static']
