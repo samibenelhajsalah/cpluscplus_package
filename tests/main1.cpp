c2c3b3551bfda37755984b9f9398a20ecfd9eb8b
BEN ELHAJ SALAH Sami <sami.bhs.91@gmail.com>
2023-05-04 22:35:58 +0200

    add test

---
+++ tests/main1.cpp
@@ -0,0 +1,11 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World, je suis dans tests\n!";
+    // tu es dans build et tu fais 
+    // /home/sami/Documents/travail/spack/var/spack/environments/compilation/.spack-env/view/bin/ccmake ../
+    // make
+    return 0;
+}
